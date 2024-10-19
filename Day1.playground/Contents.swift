77d164d1e4b616f3ae09cc161b6f5d0e4edbca36
mika.urakawa <awakaru0122@gmail.com>
2024-10-19 11:53:07 -0700

    feat: day2

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -1,3 +1,25 @@
-var a: Int
-a = 456
-a = "123"
+//var a: Int
+//a = 456
+//a = "123"
+
+//let a: Optional<Int>
+//let b: Int
+//
+//a = nil
+//b = nil
+
+//let a = 123
+//print(type(of: a))
+//
+//let b = "123"
+//print(type(of: b))
+
+//let x = 123
+//let y = 456
+//let z = max(x, y)
+
+//let x = "abc"
+//let y = "def"
+//let z = max(x, y)
+
+
